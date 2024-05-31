5acbfc6f75978f15187ac20b652d82305ee4b48e
Aaron Fulgar <gaby.tech.dev@gmail.com>
2024-05-31 22:28:15 +0800

    test app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div>
             <h1>Hello Mila Gwapa</h1>
     </body>
 </html>
