cbe227706329d08a5d7fccdf54305817e21979c1
Aaron Fulgar <gaby.tech.dev@gmail.com>
2024-06-01 19:08:54 +0800

    hello world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,6 @@
         <div>
             <h1>changesss something please</h1>
             <h1>This is it mananap</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maxime eius libero cumque saepe minus sit voluptatem incidunt quaerat aspernatur sed esse, error veritatis corrupti vero quod veniam, magnam quos quis autem ratione dolor asperiores suscipit? Quae nulla repellendus esse dolor odio quod labore ab fuga quas nemo distinctio numquam perferendis, sapiente harum, fugit doloremque neque dolore corporis dignissimos a nobis corrupti quo? Atque, odit delectus nihil minus quasi voluptas itaque, repudiandae architecto repellendus laboriosam hic ipsa ab, excepturi accusantium error! Blanditiis nemo a et officia quos. Ab dolor repudiandae sunt hic ad iste fugiat, aspernatur accusantium maxime molestias consectetur.</p>
     </body>
 </html>
